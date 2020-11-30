b16ad7db61581b6dfc719aa3ce0813ce80fc014d
Diego <diego.fernandez@bnext.es>
2020-11-30 14:13:21 +0100

    add control exceptiions and click commands in predict.py

--- {{ cookiecutter.repo_name }}/src/predict.py
+++ {{ cookiecutter.repo_name }}/src/predict.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import click
 #add to path to search interpreter parent directory
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
 
@@ -17,19 +18,27 @@ pipeline_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
 _price_pipe = load_pipeline(file_name=pipeline_file_name)
 
 
+@click.command()
+@click.option('--input_data', default= None, help='There is a data pre downloaded')
 def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
+    try:
+        data = pd.DataFrame(input_data)
+        validated_data = validate_inputs(input_data=data)
+        prediction = _price_pipe.predict(validated_data[config.FEATURES])
+        output = np.exp(prediction)
 
-    data = pd.DataFrame(input_data)
-    validated_data = validate_inputs(input_data=data)
-    prediction = _price_pipe.predict(validated_data[config.FEATURES])
-    output = np.exp(prediction)
+        results = {'predictions': output, 'version': _version}
 
-    results = {'predictions': output, 'version': _version}
+        _logger.info(
+            f'Making predictions with model version: {_version} '
+            f'Inputs: {validated_data} '
+            f'Predictions: {results}')
 
-    _logger.info(
-        f'Making predictions with model version: {_version} '
-        f'Inputs: {validated_data} '
-        f'Predictions: {results}')
+    except Exception:
+        logging.error("Fatal error in main_job", exc_info=True)
 
     return results
+
+if __name__ == '__main__':
+    sys.exit(make_prediction())
